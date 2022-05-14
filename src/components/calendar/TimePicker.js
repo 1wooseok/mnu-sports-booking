773631d7f1fceb2909af0471fc860736666ac4b1
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-14 20:22:36 +0900

    print dateState

--- src/components/calendar/TimePicker.js
+++ src/components/calendar/TimePicker.js
@@ -82,6 +82,7 @@ function TimePicker() {
 // ----- Functions -----
 async function getReservedTimeByDate(fno, dateState, dispatch) {
   if (!dateState.viewDate) return;
+  console.log({dateState});
   dispatch({ type: "LOADING" });
   try {
     const res = await getReservedTime(fno, {
