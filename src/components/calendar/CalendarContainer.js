a6641b1f2339a7ea503f4694b531f1b5618b66fa
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-14 15:06:26 +0900

    save

--- src/components/calendar/CalendarContainer.js
+++ src/components/calendar/CalendarContainer.js
@@ -2,15 +2,26 @@ import { DateContextProvider } from "../../context/dateContext";
 import DateFilter from "./DateFilter";
 import DatePicker from "./DatePicker";
 import TimePicker from "./TimePicker";
+import styled from "styled-components";
 
-function CalendarContainer() {  
+function CalendarContainer() {
   return (
     <DateContextProvider>
-      <DateFilter />
-      <DatePicker/>
-      <TimePicker />
+      <StCalendarContainer>
+        <DateFilter />
+        <DatePicker />
+        <TimePicker />
+      </StCalendarContainer>
     </DateContextProvider>
   );
 }
 
+const StCalendarContainer = styled.div`
+  width: 90vw;
+  max-width: 470px;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  border-radius: 10px;
+  padding: 10px 0;
+  font-weight: bold;
+`;
 export default CalendarContainer;
