c1796d7a3fc4e7907b8c9ce2c0f3c4fd6f441aec
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-15 21:26:13 +0900

    save

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// User
 export function getReservedTime(fno, data) {
   return axios.post(`/booking/${1}/date`, data);
 }
@@ -7,3 +8,5 @@ export function getReservedTime(fno, data) {
 export function postReserve(fno, data) {
   return axios.post(`/booking/${1}`, data);
 }
+
+// Admin
--- src/components/new-admin/AdminLogin.js
+++ src/components/new-admin/AdminLogin.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import useInputChange from '../hook/useInputs';
+import React from "react";
+import useInputChange from "../hook/useInputs";
 
 function AdminLogin() {
-  const [{id, pw}, onRset, onChange] = useInputChange({
-    id: '',
-    pw: ''
-  })
+  const [{ id, pw }, onRset, onChange] = useInputChange({
+    id: "",
+    pw: "",
+  });
 
-  console.log({ id, pw })
+  console.log({ id, pw });
   function handleSubmit(e) {
     e.preventDefault();
   }
 
   return (
     <div>
+      <h2>관리자 로그인</h2>
       <form onSubmit={handleSubmit}>
-        <input name="id" id={id} onChange={onChange} placeholder='아이디' required />
-        <input name="pw" id={pw} onChange={onChange} placeholder='비밀번호' required />
+        <div>
+          <input
+            name="id"
+            id={id}
+            onChange={onChange}
+            placeholder="아이디"
+            required
+          />
+        </div>
+        <div>
+          <input
+            name="pw"
+            id={pw}
+            onChange={onChange}
+            placeholder="비밀번호"
+            required
+          />
+        </div>
         <button>로그인</button>
       </form>
     </div>
