df4afc688ae67ef037f81119370bac7460326895
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-15 12:32:19 +0900

    deploy test

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { fullDateFormatter, timeFormatter } from "../utils/format";
 
-// const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+const API = window.location.hostname === 'localhost' ? '' : '/api';
 
 export function getReservedTime(fno, data) {
-  return axios.post(`/booking/${fno}/date`, data);
+  return axios.post(`${API}/booking/${fno}/date`, data);
 }
 
 export function postReserve(fno, dateState, userPick) {
@@ -14,5 +14,5 @@ export function postReserve(fno, dateState, userPick) {
     selectedTime: timeFormatter(userPick[0])
   };
   console.log({ data})
-  return axios.post(`/booking/${fno}`, data);
+  return axios.post(`${API}/booking/${fno}`, data);
 }
