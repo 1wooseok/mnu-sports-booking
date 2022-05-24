1d4e64eb75a7b39f4f975c336d38813c1cbcb78a
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-25 00:41:02 +0900

    fix: kakao 채팅 -> 채널 링크

--- src/apis/KakaoChannel.js
+++ src/apis/KakaoChannel.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import {KAKAO_CHANNEL_URL} from "../kakao-api";
 
 function KakaoChannel() {
   return (
-    <KakaoChatMob href="javascript:void kakaoChatStart()">
+    <KakaoChatMob href={KAKAO_CHANNEL_URL} target="_blank">
       <img src="/asset/images/KAKAO_CHANNEL.png" width="30px" height="30px" />
     </KakaoChatMob>
   );
