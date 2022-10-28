2d60cd551a713d0b3d58c76705e308e4e43c7a9c
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-29 00:46:33 +0900

    test

---
+++ src/kakao-api.js
@@ -0,0 +1,14 @@
+export const KAKAO_API_KEY = "c4b90dbf8e73d2b689639157f291268b";
+export const KAKAO_CHANNEL_URL = "https://pf.kakao.com/_ScXxib";
+export const KAKAO_HOME_ID = "_ScXxib";
+
+{/* <script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
+    <script type="module">
+      import { KAKAO_API_KEY, KAKAO_HOME_ID } from "../src/kakao-api.js";
+      Kakao.init(KAKAO_API_KEY);
+      function kakaoChatStart() {
+        Kakao.Channel.chat({
+          channelPublicId: KAKAO_HOME_ID,
+        });
+      }
+    </script> */}
